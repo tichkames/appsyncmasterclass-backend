005e10dd386ab426afa2547f7cf74eb579ed5e46
Tich <tkausiyo@gmail.com>
2022-03-26 18:42:41 +0200

    First working test

--- __tests__/steps/given.js
+++ __tests__/steps/given.js
@@ -2,9 +2,9 @@ require('dotenv').config()
 const AWS = require('aws-sdk')
 const DocumentClient = new AWS.DynamoDB.DocumentClient()
 const chance = require('chance').Chance()
-const velocityUtil = require('amplify-appsync-simulator/lib/velocity/util')
+// const velocityUtil = require('amplify-appsync-simulator/lib/velocity/util')
 
-const { RELATIONSHIPS_TABLE } = process.env
+// const { RELATIONSHIPS_TABLE } = process.env
 
 const a_random_user = () => {
   const firstName = chance.first({ nationality: 'en' })
@@ -21,87 +21,87 @@ const a_random_user = () => {
   }
 }
 
-const an_appsync_context = (identity, args, result, source, info, prev) => {
-  const util = velocityUtil.create([], new Date(), Object())
-  const context = {
-    identity,
-    args,
-    arguments: args,
-    result,
-    source,
-    info,
-    prev
-  }
-  return {
-    context,
-    ctx: context,
-    util,
-    utils: util
-  }
-}
+// const an_appsync_context = (identity, args, result, source, info, prev) => {
+//   const util = velocityUtil.create([], new Date(), Object())
+//   const context = {
+//     identity,
+//     args,
+//     arguments: args,
+//     result,
+//     source,
+//     info,
+//     prev
+//   }
+//   return {
+//     context,
+//     ctx: context,
+//     util,
+//     utils: util
+//   }
+// }
 
-const an_authenticated_user = async () => {
-  const { name, email, password } = a_random_user()
+// const an_authenticated_user = async () => {
+//   const { name, email, password } = a_random_user()
 
-  const cognito = new AWS.CognitoIdentityServiceProvider()
+//   const cognito = new AWS.CognitoIdentityServiceProvider()
 
-  const userPoolId = process.env.COGNITO_USER_POOL_ID
-  const clientId = process.env.WEB_COGNITO_USER_POOL_CLIENT_ID
+//   const userPoolId = process.env.COGNITO_USER_POOL_ID
+//   const clientId = process.env.WEB_COGNITO_USER_POOL_CLIENT_ID
 
-  const signUpResp = await cognito.signUp({
-    ClientId: clientId,
-    Username: email,
-    Password: password,
-    UserAttributes: [
-      { Name: 'name', Value: name }
-    ]
-  }).promise()
+//   const signUpResp = await cognito.signUp({
+//     ClientId: clientId,
+//     Username: email,
+//     Password: password,
+//     UserAttributes: [
+//       { Name: 'name', Value: name }
+//     ]
+//   }).promise()
 
-  const username = signUpResp.UserSub
-  console.log(`[${email}] - user has signed up [${username}]`)
+//   const username = signUpResp.UserSub
+//   console.log(`[${email}] - user has signed up [${username}]`)
 
-  await cognito.adminConfirmSignUp({
-    UserPoolId: userPoolId,
-    Username: username
-  }).promise()
+//   await cognito.adminConfirmSignUp({
+//     UserPoolId: userPoolId,
+//     Username: username
+//   }).promise()
 
-  console.log(`[${email}] - confirmed sign up`)
+//   console.log(`[${email}] - confirmed sign up`)
 
-  const auth = await cognito.initiateAuth({
-    AuthFlow: 'USER_PASSWORD_AUTH',
-    ClientId: clientId,
-    AuthParameters: {
-      USERNAME: username,
-      PASSWORD: password
-    }
-  }).promise()
+//   const auth = await cognito.initiateAuth({
+//     AuthFlow: 'USER_PASSWORD_AUTH',
+//     ClientId: clientId,
+//     AuthParameters: {
+//       USERNAME: username,
+//       PASSWORD: password
+//     }
+//   }).promise()
 
-  console.log(`[${email}] - signed in`)
+//   console.log(`[${email}] - signed in`)
 
-  return {
-    username,
-    name,
-    email,
-    idToken: auth.AuthenticationResult.IdToken,
-    accessToken: auth.AuthenticationResult.AccessToken
-  }
-}
+//   return {
+//     username,
+//     name,
+//     email,
+//     idToken: auth.AuthenticationResult.IdToken,
+//     accessToken: auth.AuthenticationResult.AccessToken
+//   }
+// }
 
-const a_user_follows_another = async (userId, otherUserId) => {
-  await DocumentClient.put({
-    TableName: RELATIONSHIPS_TABLE,
-    Item: {
-      userId,
-      sk: `FOLLOWS_${otherUserId}`,
-      otherUserId,
-      createdAt: new Date().toJSON()
-    }
-  }).promise()
-}
+// const a_user_follows_another = async (userId, otherUserId) => {
+//   await DocumentClient.put({
+//     TableName: RELATIONSHIPS_TABLE,
+//     Item: {
+//       userId,
+//       sk: `FOLLOWS_${otherUserId}`,
+//       otherUserId,
+//       createdAt: new Date().toJSON()
+//     }
+//   }).promise()
+// }
 
 module.exports = {
   a_random_user,
-  an_appsync_context,
-  an_authenticated_user,
-  a_user_follows_another,
+  // an_appsync_context,
+  // an_authenticated_user,
+  // a_user_follows_another,
 }
--- __tests__/steps/then.js
+++ __tests__/steps/then.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
-const _ = require('lodash')
+// const _ = require('lodash')
 const AWS = require('aws-sdk')
-const http = require('axios')
+// const http = require('axios')
 const fs = require('fs')
 
 const user_exists_in_UsersTable = async (id) => {
@@ -20,259 +20,259 @@ const user_exists_in_UsersTable = async (id) => {
   return resp.Item
 }
 
-const tweetsCount_is_updated_in_UsersTable = async (id, newCount) => {
-  const DynamoDB = new AWS.DynamoDB.DocumentClient()
+// const tweetsCount_is_updated_in_UsersTable = async (id, newCount) => {
+//   const DynamoDB = new AWS.DynamoDB.DocumentClient()
 
-  console.log(`looking for user [${id}] in table [${process.env.USERS_TABLE}]`)
-  const resp = await DynamoDB.get({
-    TableName: process.env.USERS_TABLE,
-    Key: {
-      id
-    }
-  }).promise()
+//   console.log(`looking for user [${id}] in table [${process.env.USERS_TABLE}]`)
+//   const resp = await DynamoDB.get({
+//     TableName: process.env.USERS_TABLE,
+//     Key: {
+//       id
+//     }
+//   }).promise()
 
-  expect(resp.Item).toBeTruthy()
-  expect(resp.Item.tweetsCount).toEqual(newCount)
+//   expect(resp.Item).toBeTruthy()
+//   expect(resp.Item.tweetsCount).toEqual(newCount)
 
-  return resp.Item
-}
+//   return resp.Item
+// }
 
-const tweet_exists_in_TweetsTable = async (id) => {
-  const DynamoDB = new AWS.DynamoDB.DocumentClient()
+// const tweet_exists_in_TweetsTable = async (id) => {
+//   const DynamoDB = new AWS.DynamoDB.DocumentClient()
 
-  console.log(`looking for tweet [${id}] in table [${process.env.TWEETS_TABLE}]`)
-  const resp = await DynamoDB.get({
-    TableName: process.env.TWEETS_TABLE,
-    Key: {
-      id
-    }
-  }).promise()
+//   console.log(`looking for tweet [${id}] in table [${process.env.TWEETS_TABLE}]`)
+//   const resp = await DynamoDB.get({
+//     TableName: process.env.TWEETS_TABLE,
+//     Key: {
+//       id
+//     }
+//   }).promise()
 
-  expect(resp.Item).toBeTruthy()
+//   expect(resp.Item).toBeTruthy()
 
-  return resp.Item
-}
+//   return resp.Item
+// }
 
-const reply_exists_in_TweetsTable = async (userId, tweetId) => {
-  const DynamoDB = new AWS.DynamoDB.DocumentClient()
+// const reply_exists_in_TweetsTable = async (userId, tweetId) => {
+//   const DynamoDB = new AWS.DynamoDB.DocumentClient()
 
-  console.log(`looking for reply by [${userId}] to [${tweetId}] in table [${process.env.TWEETS_TABLE}]`)
-  const resp = await DynamoDB.query({
-    TableName: process.env.TWEETS_TABLE,
-    IndexName: 'repliesForTweet',
-    KeyConditionExpression: 'inReplyToTweetId = :tweetId',
-    ExpressionAttributeValues: {
-      ':userId': userId,
-      ':tweetId': tweetId
-    },
-    FilterExpression: 'creator = :userId'
-  }).promise()
+//   console.log(`looking for reply by [${userId}] to [${tweetId}] in table [${process.env.TWEETS_TABLE}]`)
+//   const resp = await DynamoDB.query({
+//     TableName: process.env.TWEETS_TABLE,
+//     IndexName: 'repliesForTweet',
+//     KeyConditionExpression: 'inReplyToTweetId = :tweetId',
+//     ExpressionAttributeValues: {
+//       ':userId': userId,
+//       ':tweetId': tweetId
+//     },
+//     FilterExpression: 'creator = :userId'
+//   }).promise()
 
-  const reply = _.get(resp, 'Items.0')
+//   const reply = _.get(resp, 'Items.0')
 
-  expect(reply).toBeTruthy()
+//   expect(reply).toBeTruthy()
 
-  return reply
-}
+//   return reply
+// }
 
-const retweet_exists_in_TweetsTable = async (userId, tweetId) => {
-  const DynamoDB = new AWS.DynamoDB.DocumentClient()
+// const retweet_exists_in_TweetsTable = async (userId, tweetId) => {
+//   const DynamoDB = new AWS.DynamoDB.DocumentClient()
 
-  console.log(`looking for retweet of [${tweetId}] in table [${process.env.TWEETS_TABLE}]`)
-  const resp = await DynamoDB.query({
-    TableName: process.env.TWEETS_TABLE,
-    IndexName: 'retweetsByCreator',
-    KeyConditionExpression: 'creator = :creator AND retweetOf = :tweetId',
-    ExpressionAttributeValues: {
-      ':creator': userId,
-      ':tweetId': tweetId
-    },
-    Limit: 1
-  }).promise()
+//   console.log(`looking for retweet of [${tweetId}] in table [${process.env.TWEETS_TABLE}]`)
+//   const resp = await DynamoDB.query({
+//     TableName: process.env.TWEETS_TABLE,
+//     IndexName: 'retweetsByCreator',
+//     KeyConditionExpression: 'creator = :creator AND retweetOf = :tweetId',
+//     ExpressionAttributeValues: {
+//       ':creator': userId,
+//       ':tweetId': tweetId
+//     },
+//     Limit: 1
+//   }).promise()
 
-  const retweet = _.get(resp, 'Items.0')
+//   const retweet = _.get(resp, 'Items.0')
 
-  expect(retweet).toBeTruthy()
+//   expect(retweet).toBeTruthy()
 
-  return retweet
-}
+//   return retweet
+// }
 
-const retweet_does_not_exist_in_TweetsTable = async (userId, tweetId) => {
-  const DynamoDB = new AWS.DynamoDB.DocumentClient()
+// const retweet_does_not_exist_in_TweetsTable = async (userId, tweetId) => {
+//   const DynamoDB = new AWS.DynamoDB.DocumentClient()
 
-  console.log(`looking for retweet of [${tweetId}] in table [${process.env.TWEETS_TABLE}]`)
-  const resp = await DynamoDB.query({
-    TableName: process.env.TWEETS_TABLE,
-    IndexName: 'retweetsByCreator',
-    KeyConditionExpression: 'creator = :creator AND retweetOf = :tweetId',
-    ExpressionAttributeValues: {
-      ':creator': userId,
-      ':tweetId': tweetId
-    },
-    Limit: 1
-  }).promise()
+//   console.log(`looking for retweet of [${tweetId}] in table [${process.env.TWEETS_TABLE}]`)
+//   const resp = await DynamoDB.query({
+//     TableName: process.env.TWEETS_TABLE,
+//     IndexName: 'retweetsByCreator',
+//     KeyConditionExpression: 'creator = :creator AND retweetOf = :tweetId',
+//     ExpressionAttributeValues: {
+//       ':creator': userId,
+//       ':tweetId': tweetId
+//     },
+//     Limit: 1
+//   }).promise()
 
-  expect(resp.Items).toHaveLength(0)
+//   expect(resp.Items).toHaveLength(0)
 
-  return null
-}
+//   return null
+// }
 
-const retweet_exists_in_RetweetsTable = async (userId, tweetId) => {
-  const DynamoDB = new AWS.DynamoDB.DocumentClient()
+// const retweet_exists_in_RetweetsTable = async (userId, tweetId) => {
+//   const DynamoDB = new AWS.DynamoDB.DocumentClient()
 
-  console.log(`looking for retweet of [${tweetId}] for user [${userId}] in table [${process.env.RETWEETS_TABLE}]`)
-  const resp = await DynamoDB.get({
-    TableName: process.env.RETWEETS_TABLE,
-    Key: {
-      userId,
-      tweetId
-    }
-  }).promise()
+//   console.log(`looking for retweet of [${tweetId}] for user [${userId}] in table [${process.env.RETWEETS_TABLE}]`)
+//   const resp = await DynamoDB.get({
+//     TableName: process.env.RETWEETS_TABLE,
+//     Key: {
+//       userId,
+//       tweetId
+//     }
+//   }).promise()
 
-  expect(resp.Item).toBeTruthy()
+//   expect(resp.Item).toBeTruthy()
 
-  return resp.Item
-}
+//   return resp.Item
+// }
 
-const retweet_does_not_exist_in_RetweetsTable = async (userId, tweetId) => {
-  const DynamoDB = new AWS.DynamoDB.DocumentClient()
+// const retweet_does_not_exist_in_RetweetsTable = async (userId, tweetId) => {
+//   const DynamoDB = new AWS.DynamoDB.DocumentClient()
 
-  console.log(`looking for retweet of [${tweetId}] for user [${userId}] in table [${process.env.RETWEETS_TABLE}]`)
-  const resp = await DynamoDB.get({
-    TableName: process.env.RETWEETS_TABLE,
-    Key: {
-      userId,
-      tweetId
-    }
-  }).promise()
+//   console.log(`looking for retweet of [${tweetId}] for user [${userId}] in table [${process.env.RETWEETS_TABLE}]`)
+//   const resp = await DynamoDB.get({
+//     TableName: process.env.RETWEETS_TABLE,
+//     Key: {
+//       userId,
+//       tweetId
+//     }
+//   }).promise()
 
-  expect(resp.Item).not.toBeTruthy()
+//   expect(resp.Item).not.toBeTruthy()
 
-  return resp.Item
-}
+//   return resp.Item
+// }
 
-const tweet_exists_in_TimelinesTable = async (userId, tweetId) => {
-  const DynamoDB = new AWS.DynamoDB.DocumentClient()
+// const tweet_exists_in_TimelinesTable = async (userId, tweetId) => {
+//   const DynamoDB = new AWS.DynamoDB.DocumentClient()
 
-  console.log(`looking for tweet [${tweetId}] for user [${userId}] in table [${process.env.TIMELINES_TABLE}]`)
-  const resp = await DynamoDB.get({
-    TableName: process.env.TIMELINES_TABLE,
-    Key: {
-      userId,
-      tweetId
-    }
-  }).promise()
+//   console.log(`looking for tweet [${tweetId}] for user [${userId}] in table [${process.env.TIMELINES_TABLE}]`)
+//   const resp = await DynamoDB.get({
+//     TableName: process.env.TIMELINES_TABLE,
+//     Key: {
+//       userId,
+//       tweetId
+//     }
+//   }).promise()
 
-  expect(resp.Item).toBeTruthy()
+//   expect(resp.Item).toBeTruthy()
 
-  return resp.Item
-}
+//   return resp.Item
+// }
 
-const tweet_does_not_exist_in_TimelinesTable = async (userId, tweetId) => {
-  const DynamoDB = new AWS.DynamoDB.DocumentClient()
+// const tweet_does_not_exist_in_TimelinesTable = async (userId, tweetId) => {
+//   const DynamoDB = new AWS.DynamoDB.DocumentClient()
 
-  console.log(`looking for tweet [${tweetId}] for user [${userId}] in table [${process.env.TIMELINES_TABLE}]`)
-  const resp = await DynamoDB.get({
-    TableName: process.env.TIMELINES_TABLE,
-    Key: {
-      userId,
-      tweetId
-    }
-  }).promise()
+//   console.log(`looking for tweet [${tweetId}] for user [${userId}] in table [${process.env.TIMELINES_TABLE}]`)
+//   const resp = await DynamoDB.get({
+//     TableName: process.env.TIMELINES_TABLE,
+//     Key: {
+//       userId,
+//       tweetId
+//     }
+//   }).promise()
 
-  expect(resp.Item).not.toBeTruthy()
+//   expect(resp.Item).not.toBeTruthy()
 
-  return resp.Item
-}
+//   return resp.Item
+// }
 
-const there_are_N_tweets_in_TimelinesTable = async (userId, n) => {
-  const DynamoDB = new AWS.DynamoDB.DocumentClient()
+// const there_are_N_tweets_in_TimelinesTable = async (userId, n) => {
+//   const DynamoDB = new AWS.DynamoDB.DocumentClient()
 
-  console.log(`looking for [${n}] tweets for user [${userId}] in table [${process.env.TIMELINES_TABLE}]`)
-  const resp = await DynamoDB.query({
-    TableName: process.env.TIMELINES_TABLE,
-    KeyConditionExpression: 'userId = :userId',
-    ExpressionAttributeValues: {
-      ':userId': userId
-    },
-    ScanIndexForward: false
-  }).promise()
+//   console.log(`looking for [${n}] tweets for user [${userId}] in table [${process.env.TIMELINES_TABLE}]`)
+//   const resp = await DynamoDB.query({
+//     TableName: process.env.TIMELINES_TABLE,
+//     KeyConditionExpression: 'userId = :userId',
+//     ExpressionAttributeValues: {
+//       ':userId': userId
+//     },
+//     ScanIndexForward: false
+//   }).promise()
 
-  expect(resp.Items).toHaveLength(n)
+//   expect(resp.Items).toHaveLength(n)
 
-  return resp.Items
-}
+//   return resp.Items
+// }
 
-const there_are_N_messages_in_DirectMessagesTable = async (conversationId, n) => {
-  const DynamoDB = new AWS.DynamoDB.DocumentClient()
+// const there_are_N_messages_in_DirectMessagesTable = async (conversationId, n) => {
+//   const DynamoDB = new AWS.DynamoDB.DocumentClient()
 
-  console.log(`looking for direct messages for [conversation: ${conversationId}] in table ${process.env.DIRECT_MESSAGES_TABLE}...`)
-  const resp = await DynamoDB.query({
-    TableName: process.env.DIRECT_MESSAGES_TABLE,
-    KeyConditionExpression: 'conversationId = :conversationId',
-    ExpressionAttributeValues: {
-      ':conversationId': conversationId
-    }
-  }).promise()
+//   console.log(`looking for direct messages for [conversation: ${conversationId}] in table ${process.env.DIRECT_MESSAGES_TABLE}...`)
+//   const resp = await DynamoDB.query({
+//     TableName: process.env.DIRECT_MESSAGES_TABLE,
+//     KeyConditionExpression: 'conversationId = :conversationId',
+//     ExpressionAttributeValues: {
+//       ':conversationId': conversationId
+//     }
+//   }).promise()
 
-  expect(resp.Items).toHaveLength(n)
+//   expect(resp.Items).toHaveLength(n)
 
-  return resp.Items
-}
+//   return resp.Items
+// }
 
-const conversation_exists_in_ConversationsTable = async (userId, otherUserId) => {
-  const DynamoDB = new AWS.DynamoDB.DocumentClient()
+// const conversation_exists_in_ConversationsTable = async (userId, otherUserId) => {
+//   const DynamoDB = new AWS.DynamoDB.DocumentClient()
 
-  console.log(`looking for conversation between [${userId}] and [${otherUserId}] in table ${process.env.CONVERSATIONS_TABLE}...`)
-  const resp = await DynamoDB.get({
-    TableName: process.env.CONVERSATIONS_TABLE,
-    Key: {
-      userId,
-      otherUserId
-    }
-  }).promise()
+//   console.log(`looking for conversation between [${userId}] and [${otherUserId}] in table ${process.env.CONVERSATIONS_TABLE}...`)
+//   const resp = await DynamoDB.get({
+//     TableName: process.env.CONVERSATIONS_TABLE,
+//     Key: {
+//       userId,
+//       otherUserId
+//     }
+//   }).promise()
 
-  expect(resp.Item).toBeTruthy()
+//   expect(resp.Item).toBeTruthy()
 
-  return resp.Item
-}
+//   return resp.Item
+// }
 
-const user_can_upload_image_to_url = async (url, filepath, contentType) => {
-  const data = fs.readFileSync(filepath)
-  await http({
-    method: 'put',
-    url,
-    headers: {
-      'Content-Type': contentType
-    },
-    data
-  })
+// const user_can_upload_image_to_url = async (url, filepath, contentType) => {
+//   const data = fs.readFileSync(filepath)
+//   await http({
+//     method: 'put',
+//     url,
+//     headers: {
+//       'Content-Type': contentType
+//     },
+//     data
+//   })
 
-  console.log('uploaded image to', url)
-}
+//   console.log('uploaded image to', url)
+// }
 
-const user_can_download_image_from = async (url) => {
-  const resp = await http(url)
+// const user_can_download_image_from = async (url) => {
+//   const resp = await http(url)
 
-  console.log('downloaded image from', url)
+//   console.log('downloaded image from', url)
 
-  return resp.data
-}
+//   return resp.data
+// }
 
 module.exports = {
   user_exists_in_UsersTable,
-  tweetsCount_is_updated_in_UsersTable,
-  tweet_exists_in_TweetsTable,
-  tweet_exists_in_TimelinesTable,
-  tweet_does_not_exist_in_TimelinesTable,
-  reply_exists_in_TweetsTable,
-  retweet_exists_in_TweetsTable,
-  retweet_does_not_exist_in_TweetsTable,
-  retweet_exists_in_RetweetsTable,
-  retweet_does_not_exist_in_RetweetsTable,
-  there_are_N_tweets_in_TimelinesTable,
-  there_are_N_messages_in_DirectMessagesTable,
-  conversation_exists_in_ConversationsTable,
-  user_can_upload_image_to_url,
-  user_can_download_image_from
+//   tweetsCount_is_updated_in_UsersTable,
+//   tweet_exists_in_TweetsTable,
+//   tweet_exists_in_TimelinesTable,
+//   tweet_does_not_exist_in_TimelinesTable,
+//   reply_exists_in_TweetsTable,
+//   retweet_exists_in_TweetsTable,
+//   retweet_does_not_exist_in_TweetsTable,
+//   retweet_exists_in_RetweetsTable,
+//   retweet_does_not_exist_in_RetweetsTable,
+//   there_are_N_tweets_in_TimelinesTable,
+//   there_are_N_messages_in_DirectMessagesTable,
+//   conversation_exists_in_ConversationsTable,
+//   user_can_upload_image_to_url,
+//   user_can_download_image_from
 }
